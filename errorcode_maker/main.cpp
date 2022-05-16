cf7e01a7621144d54779cf7603b2b8024186e64f
Uptight <72242372+UptightSL@users.noreply.github.com>
2022-05-16 12:34:14 -0400

    Formatting

--- errorcode_maker/main.cpp
+++ errorcode_maker/main.cpp
@@ -3,14 +3,14 @@
 
 using namespace std;
 
-string errorcode_failedtoload = "ERR_FTL";
-string errorcode_printTableContentsNotUseable = "ERR_PTCNU";
-string errorcode_MinCriticalStackOvermax = "ERR_MCSO";
-string errorcode_MaxCriticalStackOvermax = "ERR_MXSO";
+string errorcode_failedtoload =                   "ERR_FTL";
+string errorcode_printTableContentsNotUseable =   "ERR_PTCNU";
+string errorcode_MinCriticalStackOverlimit =      "ERR_MCSO";
+string errorcode_MaxCriticalStackOverlimit =      "ERR_XCSO";
 
 int main() {
-    std::cout << &errorcode_failedtoload << std::endl;
-    std::cout << &errorcode_printTableContentsNotUseable << std::endl;
-    std::cout << &errorcode_MinCriticalStackOvermax << std::endl;
-    std::cout << &errorcode_MaxCriticalStackOvermax << std::endl;
+    std::cout << "FailedToLoad => " << &errorcode_failedtoload << std::endl;
+    std::cout << "printTableContentsNotUseable => " << &errorcode_printTableContentsNotUseable << std::endl;
+    std::cout << "MinCriticalStackOverlimit => " << &errorcode_MinCriticalStackOverlimit << std::endl;
+    std::cout << "MaxCtiticalStackOverlimit => " << &errorcode_MaxCriticalStackOverlimit << std::endl;
 }
