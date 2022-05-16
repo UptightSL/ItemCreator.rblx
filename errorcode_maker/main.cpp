5c2a3d3dd8224502385db1025f7b515bb5a2668b
Uptight <72242372+UptightSL@users.noreply.github.com>
2022-05-16 11:18:27 -0400

    Added `FailedToLoad`

---
+++ errorcode_maker/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string errorcode_failedtoload = "ERRFTL";
+
+int main() {
+    std::cout << &errorcode_failedtoload << std::endl;
+}
