6acde53b7a2e74958b308b388143f59f352fb6c1
Uptight <72242372+UptightSL@users.noreply.github.com>
2022-05-16 11:42:47 -0400

    Added `PTCNU` and `MCSO`

--- errorcode_maker/main.cpp
+++ errorcode_maker/main.cpp
@@ -3,8 +3,12 @@
 
 using namespace std;
 
-string errorcode_failedtoload = "ERRFTL";
+string errorcode_failedtoload = "ERR_FTL";
+string errorcode_printTableContentsNotUseable = "ERR_PTCNU";
+string errorcode_MinCriticalStackOvermax = "ERR_MCSO";
 
 int main() {
     std::cout << &errorcode_failedtoload << std::endl;
+    std::cout << &errorcode_printTableContentsNotUseable << std::endl;
+    std::cout << &errorcode_MinCriticalStackOvermax << std::endl;
 }
