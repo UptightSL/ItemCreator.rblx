ce937ae731ee7246bc2a1f421a26077f56fbd50c
Uptight <72242372+UptightSL@users.noreply.github.com>
2022-05-16 12:25:50 -0400

    Added `ERR_MXSO`

--- errorcode_maker/main.cpp
+++ errorcode_maker/main.cpp
@@ -6,9 +6,11 @@ using namespace std;
 string errorcode_failedtoload = "ERR_FTL";
 string errorcode_printTableContentsNotUseable = "ERR_PTCNU";
 string errorcode_MinCriticalStackOvermax = "ERR_MCSO";
+string errorcode_MaxCriticalStackOvermax = "ERR_MXSO";
 
 int main() {
     std::cout << &errorcode_failedtoload << std::endl;
     std::cout << &errorcode_printTableContentsNotUseable << std::endl;
     std::cout << &errorcode_MinCriticalStackOvermax << std::endl;
+    std::cout << &errorcode_MaxCriticalStackOvermax << std::endl;
 }
