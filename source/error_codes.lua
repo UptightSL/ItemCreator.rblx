76bf7b431bebb7bac5ca782f74f3746d8cb6e731
Uptight <72242372+UptightSL@users.noreply.github.com>
2022-05-17 07:36:48 -0400

    Added Table for Errorcodes

---
+++ source/error_codes.lua
@@ -0,0 +1,6 @@
+err = {
+    ["FailedToLoad"] = "0x7fffffffeb00"; --> Failed to Load
+    ["ERR_PTCNU"] = "0x7fffffffeb20"; --> printTableContentsNotUseable
+    ["ERR_MCSO"] = "0x7fffffffeb40"; --> MinCriticalStackOverlimit
+    ["ERR_XCSO"] = "0x7fffffffeb60"; --> MaxCriticalStackOverlimit
+}
