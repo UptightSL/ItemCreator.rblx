ed53c9ce2dfe278fb7b3b67d9067df8244a3284c
Uptight <72242372+UptightSL@users.noreply.github.com>
2022-05-17 08:47:59 -0400

    Added `ERR_ICAL`

--- errorcodes_/main.cpp
+++ errorcodes_/main.cpp
@@ -6,9 +6,11 @@ int main() {
     std::string errorcode_printTableContentsNotUseable = "ERR_PTCNU";
     std::string errorcode_MinCriticalStackOverlimit = "ERR_MCSO";
     std::string errorcode_MaxCriticalStackOverlimit = "ERR_XCSO";
+    std::string errorcode_ItemCreatorAlreadyLoaded = "ERR_ICAL";
 
-    std::cout << "FailedToLoad => " << &errorcode_failedtoload;
-    std::cout << "printTableContentsNotUseable => " << &errorcode_printTableContentsNotUseable ;
-    std::cout << "MinCriticalStackOverlimit => " << &errorcode_MinCriticalStackOverlimit;
-    std::cout << "MaxCtiticalStackOverlimit => " << &errorcode_MaxCriticalStackOverlimit;
+    std::cout << "FailedToLoad => " << &errorcode_failedtoload << "\n";
+    std::cout << "printTableContentsNotUseable => " << &errorcode_printTableContentsNotUseable << "\n";
+    std::cout << "MinCriticalStackOverlimit => " << &errorcode_MinCriticalStackOverlimit << "\n";
+    std::cout << "MaxCtiticalStackOverlimit => " << &errorcode_MaxCriticalStackOverlimit << "\n";
+    std::cout << "ItemCreatorAlreadyLoaded => " << &errorcode_ItemCreatorAlreadyLoaded << "\n";
 }
